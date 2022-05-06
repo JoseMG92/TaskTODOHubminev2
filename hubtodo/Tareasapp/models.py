c415f54daef59596a5c87ac17bcd5e5c3d67b36d
JoseMG92 <ppmezag@gmail.com>
2022-05-06 14:37:17 -0500

    corrección en models y urls en tareasapp

--- hubtodo/Tareasapp/models.py
+++ hubtodo/Tareasapp/models.py
@@ -3,30 +3,6 @@ from django.db import models
 from authentication.models import Usuarios
 
 # Create your models here.
-"""
-class Usuarios(AbstractUser):
-    
-        Profile user model
-    
-    email = models.EmailField(max_length=150, unique=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'password']
-    def get_full_name_user(self):
-        return {'full_name': f"{self.first_name} {self.last_name}"}
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
-
-class Usuarios(models.Model):
-    name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    """
-
-
 class Priority(models.Model):
     priorityName = models.CharField(max_length=50)
 
--- hubtodo/Tareasapp/urls.py
+++ hubtodo/Tareasapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Tareasapp import views
 
 urlpatterns = [
-    path('tasks', views.tareas_api),
+    path('tasks/', views.tareas_api),
     #path('auth/register',views.UsuarioView.as_view(), name='usuarios_list'),
     path('tasks/delete/<id>',views.tareas_api)
 ]
